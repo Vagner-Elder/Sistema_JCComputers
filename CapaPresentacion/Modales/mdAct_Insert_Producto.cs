63d4ae48d027daa8c03e99abae8d4f8cdbfec058
Vagner-Elder <velascozeballos@gmail.com>
2024-05-20 17:42:23 -0400

    Seguir mejorando codigo de ComboBox

--- CapaPresentacion/Modales/mdAct_Insert_Producto.cs
+++ CapaPresentacion/Modales/mdAct_Insert_Producto.cs
@@ -35,21 +35,39 @@ namespace CapaPresentacion.Modales
 
         public void SetDatos(int Id, string Serial, string TipoProducto, string Marca, string Modelo, string Capacidad, string TipoComponente, string Stock, string PrecioCompra, string PrecioVenta, string Sucursal, string Estado, string Descripcion, string FechaRegistro)
         {
+            //txtid.Text = Id.ToString();
+            //txtcodigo.Text = Serial.ToString();
+            //cboTipoProducto.Text = TipoProducto.ToString();
+            //cboMarca.Text = Marca.ToString();
+            //cboModelo.Text = Modelo.ToString();
+            //cboCapacidadTamanio.Text = Capacidad.ToString();
+            //cboTipoComponente.Text = TipoComponente.ToString();
+            //cboTipoComponente.SelectedValue = TipoComponente.ToString();
+            //txtStock.Text = Stock.ToString();
+            //txtPrecioCompra.Text = PrecioCompra.ToString();
+            //txtPrecioVenta.Text = PrecioVenta.ToString();
+            //cboSucursal.Text = Sucursal.ToString();
+            //cboestado.Text = Estado.ToString();
+            //txtDescripcion.Text = Descripcion.ToString();
+            //dtFechaRegistro.Text = FechaRegistro.ToString();
+
             txtid.Text = Id.ToString();
-            txtcodigo.Text = Serial.ToString();
-            cboTipoProducto.Text = TipoProducto.ToString();
-            cboMarca.Text = Marca.ToString();
-            cboModelo.Text = Modelo.ToString();
-            cboCapacidadTamanio.Text = Capacidad.ToString();
-            cboTipoComponente.Text = TipoComponente.ToString();
-            cboTipoComponente.SelectedValue = TipoComponente.ToString();
-            txtStock.Text = Stock.ToString();
-            txtPrecioCompra.Text = PrecioCompra.ToString();
-            txtPrecioVenta.Text = PrecioVenta.ToString();
-            cboSucursal.Text = Sucursal.ToString();
-            cboestado.Text = Estado.ToString();
-            txtDescripcion.Text = Descripcion.ToString();
-            dtFechaRegistro.Text = FechaRegistro.ToString();
+            txtcodigo.Text = Serial;
+
+            // Selección basada en nombres
+            cboTipoProducto.SelectedItem = cboTipoProducto.Items.Cast<string>().FirstOrDefault(item => item == TipoProducto);
+            cboMarca.SelectedItem = cboMarca.Items.Cast<string>().FirstOrDefault(item => item == Marca);
+            cboModelo.SelectedItem = cboModelo.Items.Cast<string>().FirstOrDefault(item => item == Modelo);
+            cboCapacidadTamanio.SelectedItem = cboCapacidadTamanio.Items.Cast<string>().FirstOrDefault(item => item == Capacidad);
+            cboTipoComponente.SelectedItem = cboTipoComponente.Items.Cast<string>().FirstOrDefault(item => item == TipoComponente);
+            cboSucursal.SelectedItem = cboSucursal.Items.Cast<string>().FirstOrDefault(item => item == Sucursal);
+
+            txtStock.Text = Stock;
+            txtPrecioCompra.Text = PrecioCompra;
+            txtPrecioVenta.Text = PrecioVenta;
+            cboestado.SelectedItem = Estado;
+            txtDescripcion.Text = Descripcion;
+            dtFechaRegistro.Text = FechaRegistro;
 
         }
 
@@ -57,6 +75,78 @@ namespace CapaPresentacion.Modales
         {
             try
             {
+                //List<Tipo_Producto> TipoProducto = objc_TipoProducto.Listar();
+                //List<Marca> marcas = objcd_Marca.Listar();
+                //List<Modelo> modelos = objcd_Modelo.Listar();
+                //List<Capacidad_Tamanio> capacidad = objcd_CapTam.Listar();
+                //List<Tipo_Componente> tipoComponente = objcd_TipComponente.Listar();
+                //List<Sucursal> sucursales = objc_Sucursal.Listar();
+
+                //cboTipoProducto.Items.Clear();
+                //cboMarca.Items.Clear();
+                //cboModelo.Items.Clear();
+                //cboCapacidadTamanio.Items.Clear();
+                //cboTipoComponente.Items.Clear();
+                //cboSucursal.Items.Clear();
+
+                //foreach (Tipo_Producto tipoProducto in TipoProducto)
+                //{
+                //    cboTipoProducto.Items.Add(tipoProducto.Nombre);
+                //}
+                //if (cboTipoProducto.Items.Count > 0)
+                //{
+                //    cboTipoProducto.SelectedIndex = 0;
+                //}
+
+                //foreach (Marca marca in marcas)
+                //{
+                //    cboMarca.Items.Add(marca.Nombre);
+                //}
+                //if (cboMarca.Items.Count > 0)
+                //{
+                //    cboMarca.SelectedIndex = 0;
+                //}
+
+
+                //foreach (Modelo modelo in modelos)
+                //{
+                //    cboModelo.Items.Add(modelo.Nombre);
+                //}
+                //if (cboModelo.Items.Count > 0)
+                //{
+                //    cboModelo.SelectedIndex = 0;
+                //}
+
+
+                //foreach (Capacidad_Tamanio capTam in capacidad)
+                //{
+                //    cboCapacidadTamanio.Items.Add(capTam.Nombre);
+                //}
+                //if (cboCapacidadTamanio.Items.Count > 0)
+                //{
+                //    cboCapacidadTamanio.SelectedIndex = 0;
+                //}
+
+
+                //foreach (Tipo_Componente tipcompo in tipoComponente)
+                //{
+                //    cboTipoComponente.Items.Add(tipcompo.Nombre);
+                //}
+                //if (cboTipoComponente.Items.Count > 0)
+                //{
+                //    cboTipoComponente.SelectedIndex = 0;
+                //}
+
+                //foreach (Sucursal sucursal in sucursales)
+                //{
+                //    cboSucursal.Items.Add(sucursal.Nombre);
+                //}
+                //if (cboSucursal.Items.Count > 0)
+                //{
+                //    cboSucursal.SelectedIndex = 0;
+                //}
+
+
                 List<Tipo_Producto> TipoProducto = objc_TipoProducto.Listar();
                 List<Marca> marcas = objcd_Marca.Listar();
                 List<Modelo> modelos = objcd_Modelo.Listar();
@@ -71,62 +161,19 @@ namespace CapaPresentacion.Modales
                 cboTipoComponente.Items.Clear();
                 cboSucursal.Items.Clear();
 
-                foreach (Tipo_Producto tipoProducto in TipoProducto)
-                {
-                    cboTipoProducto.Items.Add(tipoProducto.Nombre);
-                }
-                if (cboTipoProducto.Items.Count > 0)
-                {
-                    cboTipoProducto.SelectedIndex = 0;
-                }
+                cboTipoProducto.Items.AddRange(TipoProducto.Select(tp => tp.Nombre).ToArray());
+                cboMarca.Items.AddRange(marcas.Select(m => m.Nombre).ToArray());
+                cboModelo.Items.AddRange(modelos.Select(m => m.Nombre).ToArray());
+                cboCapacidadTamanio.Items.AddRange(capacidad.Select(ct => ct.Nombre).ToArray());
+                cboTipoComponente.Items.AddRange(tipoComponente.Select(tc => tc.Nombre).ToArray());
+                cboSucursal.Items.AddRange(sucursales.Select(s => s.Nombre).ToArray());
 
-                foreach (Marca marca in marcas)
-                {
-                    cboMarca.Items.Add(marca.Nombre);
-                }
-                if (cboMarca.Items.Count > 0)
-                {
-                    cboMarca.SelectedIndex = 0;
-                }
-
-
-                foreach (Modelo modelo in modelos)
-                {
-                    cboModelo.Items.Add(modelo.Nombre);
-                }
-                if (cboModelo.Items.Count > 0)
-                {
-                    cboModelo.SelectedIndex = 0;
-                }
-
-
-                foreach (Capacidad_Tamanio capTam in capacidad)
-                {
-                    cboCapacidadTamanio.Items.Add(capTam.Nombre);
-                }
-                if (cboCapacidadTamanio.Items.Count > 0)
-                {
-                    cboCapacidadTamanio.SelectedIndex = 0;
-                }
-
-
-                foreach (Tipo_Componente tipcompo in tipoComponente)
-                {
-                    cboTipoComponente.Items.Add(tipcompo.Nombre);
-                }
-                if (cboTipoComponente.Items.Count > 0)
-                {
-                    cboTipoComponente.SelectedIndex = 0;
-                }
-
-                foreach (Sucursal sucursal in sucursales)
-                {
-                    cboSucursal.Items.Add(sucursal.Nombre);
-                }
-                if (cboSucursal.Items.Count > 0)
-                {
-                    cboSucursal.SelectedIndex = 0;
-                }
+                cboTipoProducto.SelectedIndex = -1;
+                cboMarca.SelectedIndex = -1;
+                cboModelo.SelectedIndex = -1;
+                cboCapacidadTamanio.SelectedIndex = -1;
+                cboTipoComponente.SelectedIndex = -1;
+                cboSucursal.SelectedIndex = -1;
 
             }
             catch (Exception ex)
